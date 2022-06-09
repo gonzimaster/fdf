db8e1e782ebca3f32d4a9b4d1a8675175105ad6c
gonzimaster <oscarpunticoma@gmail.com>
2022-06-09 12:14:01 +0200

    Delete redundant list

--- inc/parse_map.h
+++ inc/parse_map.h
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 10:26:17 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/09 10:53:55 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/09 12:10:54 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,4 @@ typedef struct s_coord
 }						*t_coord;
 
 void	ft_parse_map(char *map_path, t_coord *coord, unsigned int *map_size);
-void	ft_create_list(t_coord *coord, t_list **lst, unsigned int *map_size);
 #endif
--- src/fdf/fdf.c
+++ src/fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 08:41:52 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/09 11:49:40 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/09 12:13:43 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,6 @@
 #include "parse_map.h"
 #include "utils.h"
 #include <stdio.h>
-
-static void	print_list(t_list **lst)
-{
-	t_list	*current_node;
-
-	current_node = *lst;
-	while (current_node)
-	{
-		printf("(%d %d %d)\n", ((t_coord)current_node->content)->x,
-								((t_coord)current_node->content)->y,
-								((t_coord)current_node->content)->z);
-		current_node = current_node->next;;
-	}
-}
-
-static void	ft_free_list(t_list **lst)
-{
-	t_list *tmp;
-
-	while (*lst)
-	{
-		tmp = *lst;
-		*lst = (*lst)->next;
-		free(tmp);
-	}
-}
 
 static void	ft_allocate_coord(t_coord *coord)
 {
@@ -52,7 +26,6 @@ static void	ft_allocate_coord(t_coord *coord)
 int	main(int argc, char *argv[])
 {
 	t_coord			coord;
-	t_list			*lst;
 	unsigned int	map_size;
 
 	if (argc != 2)
@@ -60,9 +33,12 @@ int	main(int argc, char *argv[])
 	map_size = 0;
 	ft_allocate_coord(&coord);
 	ft_parse_map(argv[1], &coord, &map_size);
-	ft_create_list(&coord, &lst, &map_size);
-	print_list(&lst);
-	ft_free_list(&lst);
+	/*
+	unsigned int	i = -1;
+	while (++i < map_size)
+		printf("(%d, %d, %d) ", coord[i].x, coord[i].y, coord[i].z);
+	printf("\n");
+	*/
 	free(coord);
 	return (0);
 }
