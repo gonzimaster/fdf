b1fe8edecd49b28fd26869be61b6fbd38e164ad8
Oscar Gonzalez simon <ogonzale@cbr4s5.42barcelona.com>
2022-06-10 18:27:30 +0200

    some problems with iso projecion

--- inc/scale_coord.h
+++ inc/scale_coord.h
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 19:06:44 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/10 13:50:49 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/10 17:41:52 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@ typedef struct		s_dim
 	int				min_elevation;
 	int				max_elevation;
 }					t_dim;
+
+typedef struct s_lim_dim
+{
+	int	min_x;
+	int	max_x;
+	int	min_y;
+	int	max_y;
+}		t_lim_dim;
 
 void	ft_scale_and_center(t_coord *coord, unsigned int size);
 
--- src/fdf/scale_coord.c
+++ src/fdf/scale_coord.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 19:04:17 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/10 16:35:02 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/10 18:13:39 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,37 @@ static t_dim	ft_get_max_dims(t_coord *coord, unsigned int size)
 {
 	unsigned int	i;
 	t_dim			max_dims;
+	t_lim_dim		lim_coord;
 	t_coord			*tmp;
 
 	tmp = coord;
 	i = 0;
+	lim_coord.min_x = 0;
+	lim_coord.max_x = 0;
+	lim_coord.min_y = 0;
+	lim_coord.max_y = 0;
 	max_dims.width = 0;
 	max_dims.height = 0;
 	max_dims.min_elevation = 0;
 	max_dims.max_elevation = 0;
 	while (i < size)
 	{
-		if (ft_abs_value(tmp[i].x) > max_dims.width)
-			max_dims.width = tmp[i].x;
-		if (ft_abs_value(tmp[i].y) > max_dims.height)
-			max_dims.height = tmp[i].y;
+		if (tmp[i].x < lim_coord.min_x)
+			lim_coord.min_x = tmp[i].x;
+		if (tmp[i].y < lim_coord.min_y)
+			lim_coord.min_y = tmp[i].y;
+		if (tmp[i].x > lim_coord.max_x)
+			lim_coord.max_x = tmp[i].x;
+		if (tmp[i].y > lim_coord.max_y)
+			lim_coord.max_y = tmp[i].y;
 		if (tmp[i].z < max_dims.min_elevation)
 			max_dims.min_elevation = tmp[i].z;
 		if (tmp[i].z > max_dims.max_elevation)
 			max_dims.max_elevation = tmp[i].z;
 		i++;
 	}
+	max_dims.width = ft_abs_value(lim_coord.max_x - lim_coord.min_x);
+	max_dims.height = ft_abs_value(lim_coord.max_y - lim_coord.min_y);
 	return (max_dims);
 }
 
@@ -86,18 +97,21 @@ static void	ft_to_isometric(t_coord *coord, unsigned int size)
 	t_coord			tmp;
 	float			u;
 	float			v;
+	double			angle;
 
 	i = 0;
+	angle = 1;
 	while (i < size)
 	{
 		tmp.x = coord[i].x;	
 		tmp.y = coord[i].y;
+		printf("y = %d\n", tmp.y);
 		tmp.z = coord[i].z;
-		u = (tmp.x - tmp.z) / sqrt(2);
-		v = (tmp.x + 2 * tmp.y + tmp.z) / sqrt(6);
+		u = tmp.x + cos(angle) * tmp.z - cos(angle) * tmp.y;
+		v = -tmp.y * sin(angle) - tmp.z * sin(angle);
 		coord[i].x = (int)u;
 		coord[i].y = (int)v;
-		printf("%d\n", coord[i].y);
+		printf("y = %d\n", coord[i].y);
 		i++;
 	}
 }
@@ -110,5 +124,6 @@ void	ft_scale_and_center(t_coord *coord, unsigned int size)
 	ft_scale_to_fit(coord, max_dims, size);
 	ft_to_isometric(coord, size);
 	max_dims = ft_get_max_dims(coord, size);
+	printf("max dims = (%d, %d)\n", max_dims.width, max_dims.height); 
 	ft_center(coord, max_dims, size);
 }
