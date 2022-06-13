25680eb40a241f841c9bd5e3645e83de79396831
Oscar Gonzalez simon <ogonzale@cbr4s5.42barcelona.com>
2022-06-13 16:30:08 +0200

    Iso projection and centering works

--- src/fdf/scale_coord.c
+++ src/fdf/scale_coord.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 19:04:17 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/11 12:55:24 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/13 16:29:20 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ static t_dim	ft_get_max_dims(t_coord *coord, unsigned int size)
 
 	tmp = coord;
 	i = 0;
-	lim_coord.min_x = 0;
+	lim_coord.min_x = 1920;
 	lim_coord.max_x = 0;
-	lim_coord.min_y = 0;
+	lim_coord.min_y = 1080;
 	lim_coord.max_y = 0;
 	max_dims.width = 0;
 	max_dims.height = 0;
@@ -52,27 +52,15 @@ static t_dim	ft_get_max_dims(t_coord *coord, unsigned int size)
 			max_dims.max_elevation = tmp[i].z;
 		i++;
 	}
+	printf("min_x = %d | max_x = %d\n", lim_coord.min_x, lim_coord.max_x);
+	printf("min_y = %d | max_y = %d\n", lim_coord.min_y, lim_coord.max_y);
 	max_dims.width = ft_abs_value(lim_coord.max_x - lim_coord.min_x);
 	max_dims.height = ft_abs_value(lim_coord.max_y - lim_coord.min_y);
 	return (max_dims);
 }
-/*
-static void	ft_center(t_coord *coord, t_dim max_dims, unsigned int size)
-{
-	unsigned int	i;
 
-	i = 0;
-	while (i < size)
-	{
-		coord[i].x = coord[i].x + (1920 - max_dims.width) / 2;
-		coord[i].y = coord[i].y + (1080 - max_dims.height) / 2;
-		i++;
-	}
-}
-*/
-static void	ft_scale_to_fit(t_coord *coord, t_dim max_dims, t_screen screen, unsigned int size)
+static float	ft_scale_to_fit(t_dim max_dims, t_screen screen, float occ_screen)
 {
-	unsigned int	i;
 	float			scale_factor;
 	float			scale_x_factor;
 	float			scale_y_factor;
@@ -80,19 +68,13 @@ static void	ft_scale_to_fit(t_coord *coord, t_dim max_dims, t_screen screen, uns
 	scale_x_factor = max_dims.width / (float)screen.width;
 	scale_y_factor = max_dims.height / (float)screen.height;
 	if (scale_x_factor > scale_y_factor)
-		scale_factor = 0.5 / scale_x_factor;
+		scale_factor = occ_screen / scale_x_factor;
 	else
-		scale_factor = 0.5 / scale_y_factor;
-	i = 0;
-	while (i < size)
-	{
-		coord[i].x = (int)scale_factor * coord[i].x;
-		coord[i].y = (int)scale_factor * coord[i].y;
-		i++;
-	}
+		scale_factor = occ_screen / scale_y_factor;
+	return (scale_factor);;
 }
 
-static void	ft_to_isometric(t_coord *coord, t_screen screen, unsigned int size)
+static void	ft_to_isometric(t_coord *coord, t_dim max_dims, t_screen screen, unsigned int size)
 {
 	unsigned int	i;
 	float			u;
@@ -100,17 +82,19 @@ static void	ft_to_isometric(t_coord *coord, t_screen screen, unsigned int size)
 	float			v;
 	float			v_o;
 	float			angle;
+	float			occ_screen;
+	int				s;
 
 	i = 0;
 	angle = 30.0;
-	u_o = (coord[size/2].x - coord[size/2].y) * cos(ft_degree_to_rad(angle)); 
-	v_o = (coord[size/2].x + coord[size/2].y + 1) * sin(ft_degree_to_rad(angle)) - coord[size/2].z;
-	//u_o = (screen.width / 2 - screen.height / 2) * cos(ft_degree_to_rad(angle)); 
-	//v_o = (screen.width / 2 - screen.height / 2 + 1) * sin(ft_degree_to_rad(angle));
+	occ_screen = 0.5;
+	s = ft_scale_to_fit(max_dims, screen, occ_screen);
+	u_o = (max_dims.width - max_dims.height) * cos(ft_degree_to_rad(angle)) * s / 2; 
+	v_o = (max_dims.width + max_dims.height) * sin(ft_degree_to_rad(angle)) * s / 2;
 	while (i < size)
 	{
-		u = (coord[i].x - coord[i].y) * cos(ft_degree_to_rad(angle)) - u_o + screen.width / 2; 
-		v = (coord[i].x + coord[i].y) * sin(ft_degree_to_rad(angle)) - coord[i].z - v_o + screen.height / 2;
+		u = ((coord[i].x  - coord[i].y) * cos(ft_degree_to_rad(angle)) * s) - u_o + screen.width / 2; 
+		v = ((coord[i].x  + coord[i].y ) * sin(ft_degree_to_rad(angle)) * s - coord[i].z) - v_o + screen.height / 2;
 		coord[i].x = (int)u;
 		coord[i].y = (int)v;
 		i++;
@@ -122,9 +106,5 @@ void	ft_scale_and_center(t_coord *coord, t_screen screen, unsigned int size)
 	t_dim			max_dims;
 
 	max_dims = ft_get_max_dims(coord, size);
-	ft_scale_to_fit(coord, max_dims, screen, size);
-	max_dims = ft_get_max_dims(coord, size);
-	ft_to_isometric(coord, screen, size);
-	//max_dims = ft_get_max_dims(coord, size);
-	//ft_center(coord, max_dims, size);
+	ft_to_isometric(coord, max_dims, screen, size);
 }
