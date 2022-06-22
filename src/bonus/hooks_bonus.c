207ac73d63eede40b7e1473749f3d1228e46c535
Oscar Gonzalez simon <ogonzale@cbr4s5.42barcelona.com>
2022-06-22 12:54:45 +0200

    Map moves to the left

--- src/bonus/hooks_bonus.c
+++ src/bonus/hooks_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ogonzale <ogonzale@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 11:22:22 by ogonzale          #+#    #+#             */
-/*   Updated: 2022/06/21 19:20:25 by ogonzale         ###   ########.fr       */
+/*   Updated: 2022/06/22 12:53:47 by ogonzale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,19 @@ static void	ft_translate_map_left(t_vars *vars)
 
 static int	ft_key_router(int key, t_vars *vars)
 {
-	if (key == LINUX_ESC)
+	if (key == LINUX_ESC || key == 53)
 	{
 		mlx_destroy_window(vars->mlx, vars->win);
 		free(vars->map_data->coord);
 		free(vars->map_data->tr_coord);
 		exit(0);
 	}
-	if (key == RIGHT)
+	if (key == RIGHT || key == 2)
 	{
+		mlx_destroy_image(vars->mlx, vars->img->img);
+		vars->img->img = mlx_new_image(vars->mlx, vars->screen.width, vars->screen.height);
+		vars->img->addr = mlx_get_data_addr(vars->img->img, &vars->img->bits_per_pixel,
+			&vars->img->line_length, &vars->img->endian);
 		ft_translate_map_left(vars);
 		ft_print_image(vars->img, *(vars->map_data), vars->screen);
 		mlx_put_image_to_window(vars->mlx, vars->win, vars->img->img, 0, 0);
